fbd8a212b18742cafc0cb54e6311e05676ca5b77
Vũ Văn Chung <vanchung1995@gmail.com>
2024-11-26 23:26:50 +0700

    Update create-rds.sh

--- cloudformation/create-rds.sh
+++ cloudformation/create-rds.sh
@@ -2,4 +2,4 @@
 aws cloudformation create-stack --stack-name primary-rds-stack --region us-east-1 --template-body file://rds-primary.yaml --parameters file://rds-params.json
 
 # create secondary vpc stack
-aws cloudformation create-stack --stack-name secondary-rds-stack --region us-west-2 --template-body file://rds.yaml --parameters file://rds-params.json
+aws cloudformation create-stack --stack-name secondary-rds-stack --region us-west-2 --template-body file://rds-secondary.yaml --parameters file://rds-params.json
