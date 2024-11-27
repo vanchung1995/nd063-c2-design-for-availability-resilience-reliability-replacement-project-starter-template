6b91f9779df493d2b65abbc77f8fa41f6b14519f
Vũ Văn Chung <vanchung1995@gmail.com>
2024-11-27 16:52:04 +0700

    Update create-ec2.sh

--- cloudformation/create-ec2.sh
+++ cloudformation/create-ec2.sh
@@ -1,1 +1,3 @@
 aws cloudformation create-stack --stack-name primary-ec2-stack --region us-east-1 --template-body file://ec2-template.yaml
+
+aws cloudformation create-stack --stack-name secondary-ec2-stack --region us-west-2 --template-body file://ec2-template.yaml
