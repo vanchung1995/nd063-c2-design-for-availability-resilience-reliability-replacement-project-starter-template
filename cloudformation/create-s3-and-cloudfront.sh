94f8ea5132e187727dd405e47198e44fb19b07d4
Vũ Văn Chung <vanchung1995@gmail.com>
2024-11-28 09:22:27 +0700

    Update create-s3-and-cloudfront.sh

--- cloudformation/create-s3-and-cloudfront.sh
+++ cloudformation/create-s3-and-cloudfront.sh
@@ -1,1 +1,3 @@
 aws cloudformation create-stack --stack-name s3-stack --region us-west-2 --template-body file://s3-template.yaml
+
+aws cloudformation describe-stacks --stack-name s3-stack --region us-west-2 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text
