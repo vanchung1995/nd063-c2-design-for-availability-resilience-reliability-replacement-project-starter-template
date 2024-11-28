2de7fc0b53b43162f2131bd8394e4857fada6c9c
Vũ Văn Chung <vanchung1995@gmail.com>
2024-11-28 09:29:04 +0700

    Update create-s3-and-cloudfront.sh

--- cloudformation/create-s3-and-cloudfront.sh
+++ cloudformation/create-s3-and-cloudfront.sh
@@ -1,3 +1,8 @@
 aws cloudformation create-stack --stack-name s3-stack --region us-west-2 --template-body file://s3-template.yaml
 
 aws cloudformation describe-stacks --stack-name s3-stack --region us-west-2 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text
+
+export BUCKET_NAME = $(aws cloudformation describe-stacks --stack-name s3-stack --region us-west-2 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text) # ubuntu
+set BUCKET_NAME $(aws cloudformation describe-stacks --stack-name s3-stack --region us-west-2 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text) # windows
+
+aws s3 cp ../s3/* s3://$(BUCKET_NAME)/
