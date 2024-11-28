b32236b8acf0566062e3b7cbc2081c34f33f3ae1
Vũ Văn Chung <vanchung1995@gmail.com>
2024-11-28 09:54:59 +0700

    Update create-s3-and-cloudfront.sh

--- cloudformation/create-s3-and-cloudfront.sh
+++ cloudformation/create-s3-and-cloudfront.sh
@@ -6,3 +6,4 @@ export BUCKET_NAME = $(aws cloudformation describe-stacks --stack-name s3-stack 
 set BUCKET_NAME $(aws cloudformation describe-stacks --stack-name s3-stack --region us-west-2 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text) # windows
 
 aws s3 cp ../s3/ s3://$BUCKET_NAME/ --recursive 
+aws s3 rm s3://$BUCKET_NAME/winter.jpg
